7f2f2e215ba1fa59dce240a4a77b9a66d3ef154a
Jonathan Anz <98674816+Jonathan-Anz@users.noreply.github.com>
2024-11-14 15:55:01 -0800

    Implemented Archimedes approach

--- PI_Archimedes.py
+++ PI_Archimedes.py
@@ -1,9 +1,19 @@
 # This class implements the archimedes approach for generating pi
+import math
 
 class Archimedes:
 
-    # Function to calculate pi with n iterations
+    # Function to calculate pi with n iterations (sides)
+    # Formula: PI = sin(360 / 2n) * n
     def Calculate_PI(n):
 
-        # Temporary - need to implement properly
-        return 3.14
+        try:
+            value = 360 / (n * 2)
+            sinValue = math.sin(math.radians(value))
+            pi = sinValue * n
+
+            return pi
+        
+        except ZeroDivisionError:
+            print("n cannot be zero!")
+            return 0
