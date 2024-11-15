9f6975538d8f1b4be4897e5c38960700f37b6aac
Jonathan Anz <98674816+Jonathan-Anz@users.noreply.github.com>
2024-11-14 16:13:27 -0800

    Updated driver class

--- PI_Driver.py
+++ PI_Driver.py
@@ -4,6 +4,8 @@
 
 # This is the driver class to test pi computing functions
 from PI_Archimedes import Archimedes
+from PI_Leibniz import Leibniz
+from PI_MonteCarlo import MonteCarlo
 
 import math
 import time
@@ -14,13 +16,29 @@ def main():
 
     # Archimedes method
     print("Archimedes Method:")
-    archimedesPI = Archimedes.Calculate_PI(1)
+    archimedesPI = Archimedes.Calculate_PI(50)
     print("pi =", archimedesPI)
     print("Difference from real pi:", abs(math.pi - archimedesPI))
 
-    # Other methods...
+    print()
+
+    # Leibniz method
+    print("Leibniz Method:")
+    leibnizPI = Leibniz.Calculate_PI(50)
+    print("pi =", leibnizPI)
+    print("Difference from real pi:", abs(math.pi - leibnizPI))
 
     print()
+
+    # Monte Carlo method
+    print("Monte Carlo Method:")
+    monteCarloPI = MonteCarlo.Calculate_PI(50)
+    print("pi =", monteCarloPI)
+    print("Difference from real pi:", abs(math.pi - monteCarloPI))
+
+    print()
+
+    # Include more methods?
 
 if __name__ == '__main__':
     main()
