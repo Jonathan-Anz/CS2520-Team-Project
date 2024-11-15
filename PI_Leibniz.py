c7e102b68b26803a8490efeb7f3c5d6ff252a202
Jonathan Anz <98674816+Jonathan-Anz@users.noreply.github.com>
2024-11-14 16:10:53 -0800

    Delete PI_Leibniz.py

--- PI_Leibniz.py
+++
@@ -1,9 +0,0 @@
-# This class implements the leibniz formula for generating pi
-
-class Leibniz:
-
-    # Function to calculate pi with n terms
-    def Calculate_PI(n):
-
-        # Need to implement
-        return 3.14
