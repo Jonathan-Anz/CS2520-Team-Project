06a9ff5b526ca07f0206f5a8b9ed54a4b21afcc9
Jonathan Anz <98674816+Jonathan-Anz@users.noreply.github.com>
2024-11-14 16:08:00 -0800

    Added skeleton classes for leibnez and monte carlo methods.

---
+++ PI_MonteCarlo.py
@@ -0,0 +1,9 @@
+# This class implements the monte carlo method for generating pi
+
+class MonteCarlo:
+
+    # Function to calculate pi with n points
+    def Calculate_PI(n):
+
+        # Need to implement
+        return 3.14
