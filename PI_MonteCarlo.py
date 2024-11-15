94a9097fe7c16c8ca101bc7c90e4cd45e3266bc8
Jonathan Anz <98674816+Jonathan-Anz@users.noreply.github.com>
2024-11-14 16:11:06 -0800

    Delete PI_MonteCarlo.py

--- PI_MonteCarlo.py
+++
@@ -1,9 +0,0 @@
-# This class implements the monte carlo method for generating pi
-
-class MonteCarlo:
-
-    # Function to calculate pi with n points
-    def Calculate_PI(n):
-
-        # Need to implement
-        return 3.14
